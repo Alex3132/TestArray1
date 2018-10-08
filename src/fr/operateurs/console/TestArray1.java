0bee9438aa37b618ec7160da26b6300b34cdb162
Alex3132 <alexis.zueras.pro@gmail.com>
2018-10-08 10:17:09 +0200

    public to private class + optmisation

--- src/fr/operateurs/console/TestArray1.java
+++ src/fr/operateurs/console/TestArray1.java
@@ -1,7 +1,7 @@
 package fr.operateurs.console;
 
-public class TestArray1 {
-    public void test() {
+class TestArray1 {
+    void test() {
         int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
 
 
